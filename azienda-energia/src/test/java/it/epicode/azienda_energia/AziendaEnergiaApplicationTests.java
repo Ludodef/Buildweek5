e437156b9a4bb2451bc566a2c2b0101d38dd7fd7
Ludodef <defazioludovica95@gmai.com>
2024-06-03 09:43:17 +0200

    inizializzazione progetto

---
+++ azienda-energia/src/test/java/it/epicode/azienda_energia/AziendaEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.azienda_energia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AziendaEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
