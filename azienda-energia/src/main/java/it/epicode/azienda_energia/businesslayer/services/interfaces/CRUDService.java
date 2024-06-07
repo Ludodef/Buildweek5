32e9ff2ce0eb030fc0c3f859225a6dadb5dabf67
Danilon6 <danilonebulosi@gmail.com>
2024-06-07 17:07:25 +0200

    All done

--- azienda-energia/src/main/java/it/epicode/azienda_energia/businesslayer/services/interfaces/CRUDService.java
+++ azienda-energia/src/main/java/it/epicode/azienda_energia/businesslayer/services/interfaces/CRUDService.java
@@ -1,10 +1,11 @@
 package it.epicode.azienda_energia.businesslayer.services.interfaces;
 
 import it.epicode.azienda_energia.businesslayer.services.dto.BaseDTO;
+import it.epicode.azienda_energia.datalayer.entities.BaseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface CRUDService<T, A extends BaseDTO> {
+public interface CRUDService<T extends BaseEntity, A extends BaseDTO> {
     Page<T> getAll(Pageable p);
 
     T getById(Long id);
--- azienda-energia/src/main/java/it/epicode/azienda_energia/businesslayer/services/interfaces/RolesInvoiceStatusCRUD.java
+++ azienda-energia/src/main/java/it/epicode/azienda_energia/businesslayer/services/interfaces/RolesInvoiceStatusCRUD.java
@@ -1,10 +1,11 @@
 package it.epicode.azienda_energia.businesslayer.services.interfaces;
 
 import it.epicode.azienda_energia.businesslayer.services.dto.BaseDTO;
+import it.epicode.azienda_energia.datalayer.entities.BaseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface RolesInvoiceStatusCRUD<T, A extends BaseDTO> {
+public interface RolesInvoiceStatusCRUD<T extends BaseEntity, A extends BaseDTO> {
 
     Page<T> getAll(Pageable p);
 
